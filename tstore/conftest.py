3f548b20df46fe9ff4ce223b094d90a720989470
Son Pham-Ba <son.phamba@epfl.ch>
2024-05-07 10:33:11 +0200

    test: add test for df long store and load

--- tstore/conftest.py
+++ tstore/conftest.py
@@ -3,32 +3,55 @@
 from pathlib import Path
 
 import dask
+import numpy as np
 import pandas as pd
 import polars as pl
 import pyarrow as pa
 import pytest
 
+# Functions
+
+
+class Helpers:
+    """Helper functions for the tests."""
+
+    @staticmethod
+    def create_dask_dataframe():
+        """Create a Dask DataFrame with a dummy time series.
+
+        The columns are:
+            - name: str
+            - id: int
+            - x: float
+            - y: float
+        """
+        df_dask = dask.datasets.timeseries(
+            start="2000-01-01",
+            end="2000-01-03",
+            freq="1h",
+            partition_freq="1d",
+            dtypes=None,
+            seed=None,
+        )
+
+        return df_dask
+
 
 @pytest.fixture()
-def dask_dataframe() -> dask.dataframe.DataFrame:
-    """Create a Dask DataFrame with a dummy time series.
+def helpers() -> type[Helpers]:
+    """Return the class of helper functions."""
+    return Helpers
 
-    The columns are:
-        - name: str
-        - id: int
-        - x: float
-        - y: float
-    """
-    df_dask = dask.datasets.timeseries(
-        start="2000-01-01",
-        end="2000-01-03",
-        freq="1h",
-        partition_freq="1d",
-        dtypes=None,
-        seed=None,
-    )
 
-    return df_dask
+# Fixtures
+
+## DataFrames
+
+
+@pytest.fixture()
+def dask_dataframe(helpers) -> dask.dataframe.DataFrame:
+    """Create a Dask DataFrame with a dummy time series."""
+    return helpers.create_dask_dataframe()
 
 
 @pytest.fixture()
@@ -59,6 +82,9 @@ def pandas_dataframe_arrow_dtypes(arrow_dataframe: pa.Table) -> pd.DataFrame:
     return df_pd
 
 
+## Stored data
+
+
 @pytest.fixture()
 def parquet_timeseries(tmp_path: Path, dask_dataframe: dask.dataframe.DataFrame) -> Path:
     """Create a Parquet file with a dummy time series."""
@@ -86,3 +112,82 @@ def parquet_timeseries(tmp_path: Path, dask_dataframe: dask.dataframe.DataFrame)
     )
 
     return filepath
+
+
+@pytest.fixture()
+def tstore_path(tmp_path: Path, pandas_long_dataframe: pd.DataFrame) -> Path:
+    """Store a Pandas long DataFrame as a TStore."""
+    # TODO: Rewrite without using tstore to not depend on implementation
+    from tstore.tslong.pandas import to_tstore
+
+    dirpath = tmp_path / "test_tstore"
+    tstore_structure = "id-var"
+    overwrite = True
+    id_var = "store_id"
+    time_var = "time"
+    static_variables = ["static_var"]
+    # geometry = None  # NOT IMPLEMENTED YET
+
+    # Same partitioning for all TS
+    partitioning = "year/month"
+    # Partitioning specific to each TS
+    partitioning = {"precipitation": "year/month"}
+
+    # Each timeseries is a TS object
+    ts_variables = ["name", "id", "x", "y"]
+    # Group multiple timeseries into one TS object
+    ts_variables = {"precipitation": ["name", "id", "x", "y"]}
+
+    print(pandas_long_dataframe.shape)
+
+    to_tstore(
+        pandas_long_dataframe,
+        # TSTORE options
+        str(dirpath),
+        # DFLONG attributes
+        id_var=id_var,
+        time_var=time_var,
+        ts_variables=ts_variables,
+        static_variables=static_variables,
+        # TSTORE options
+        partitioning=partitioning,
+        tstore_structure=tstore_structure,
+        overwrite=overwrite,
+    )
+
+    return dirpath
+
+
+## Long DataFrames
+
+
+@pytest.fixture()
+def pandas_long_dataframe(helpers) -> pd.DataFrame:
+    """Create a long Pandas DataFrame."""
+    store_ids = np.arange(1, 4 + 1)
+
+    df_list = []
+
+    for store_id in store_ids:
+        df = helpers.create_dask_dataframe().compute()
+        df["store_id"] = store_id
+        df["static_var"] = f"{store_id}_static"
+        df_list.append(df)
+
+    df = pd.concat(df_list)
+    df["static_var"] = df["store_id"].astype(str) + "_static"
+    return df
+
+
+@pytest.fixture()
+def arrow_long_dataframe(pandas_long_dataframe: pd.DataFrame) -> pa.Table:
+    """Create a long Arrow Table."""
+    df_arrow = pa.Table.from_pandas(pandas_long_dataframe)
+    return df_arrow
+
+
+@pytest.fixture()
+def polars_long_dataframe(arrow_long_dataframe: pa.Table) -> pl.DataFrame:
+    """Create a long Polars DataFrame."""
+    df_pl = pl.from_arrow(arrow_long_dataframe)
+    return df_pl
---
+++ tstore/tests/test_tslong.py
@@ -0,0 +1,117 @@
+"""Test the tslong subpackage."""
+
+import os
+from functools import partial
+from pathlib import Path
+from typing import Callable
+
+import pandas as pd
+import polars as pl
+import pytest
+
+import tstore
+from tstore.tslong.pandas import (
+    to_tstore as to_tstore_pd,
+)
+from tstore.tslong.polars import (
+    open_tslong as open_tslong_pl,
+)
+from tstore.tslong.polars import (
+    to_tstore as to_tstore_pl,
+)
+
+# Functions ####################################################################
+
+
+def store_long_dataframe(to_tstore_func: Callable, df: pd.DataFrame, dirpath: str) -> None:
+    tstore_structure = "id-var"
+    overwrite = True
+    id_var = "store_id"
+    time_var = "time"
+    static_variables = ["static_var"]
+    # geometry = None  # NOT IMPLEMENTED YET
+
+    # Same partitioning for all TS
+    partitioning = "year/month"
+    # Partitioning specific to each TS
+    partitioning = {"precipitation": "year/month"}
+
+    # Each timeseries is a TS object
+    ts_variables = ["name", "id", "x", "y"]
+    # Group multiple timeseries into one TS object
+    ts_variables = {"precipitation": ["name", "id", "x", "y"]}
+
+    to_tstore_func(
+        df,
+        # TSTORE options
+        dirpath,
+        # DFLONG attributes
+        id_var=id_var,
+        time_var=time_var,
+        ts_variables=ts_variables,
+        static_variables=static_variables,
+        # TSTORE options
+        partitioning=partitioning,
+        tstore_structure=tstore_structure,
+        overwrite=overwrite,
+    )
+
+
+store_pandas_long_dataframe = partial(store_long_dataframe, to_tstore_pd)
+store_polars_long_dataframe = partial(store_long_dataframe, to_tstore_pl)
+
+
+# Tests ########################################################################
+
+
+@pytest.mark.parametrize(
+    ("dataframe_fixture_name", "store_long_dataframe_func"),
+    [
+        ("pandas_long_dataframe", store_pandas_long_dataframe),
+        ("polars_long_dataframe", store_polars_long_dataframe),
+    ],
+)
+def test_store(
+    tmp_path: Path,
+    dataframe_fixture_name: str,
+    store_long_dataframe_func,
+    request,
+) -> None:
+    """Test the to_tstore function."""
+    df = request.getfixturevalue(dataframe_fixture_name)
+    dirpath = tmp_path / "test_tstore"
+    store_long_dataframe_func(df, str(dirpath))
+
+    # Check that the tstore is created
+    assert dirpath.is_dir()
+
+    # Check directory content
+    assert sorted(os.listdir(dirpath)) == ["1", "2", "3", "4", "_attributes.parquet", "tstore_metadata.yml"]
+    assert os.listdir(dirpath / "1" / "precipitation" / "year=2000" / "month=1") == ["part-0.parquet"]
+
+
+class TestLoad:
+    """Test the from_tstore function."""
+
+    def test_pandas(
+        self,
+        tstore_path: Path,
+    ) -> None:
+        """Test loading as a Pandas DataFrame."""
+        tslong = tstore.open_tslong(tstore_path, ts_variables=["precipitation"])
+        assert type(tslong) is pd.DataFrame
+        assert tslong.shape == (192, 7)
+        # TODO: time column is counted
+        # TODO: line order is not preserved
+        # TODO: column order is not preserved
+
+    def test_polars(
+        self,
+        tstore_path: Path,
+    ) -> None:
+        """Test loading as a Polars DataFrame."""
+        tslong = open_tslong_pl(tstore_path, ts_variables=["precipitation"])
+        assert type(tslong) is pl.DataFrame
+        assert tslong.shape == (192, 7)
+        # TODO: time column is counted
+        # TODO: column order is not preserved
