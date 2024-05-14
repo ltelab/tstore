6bb2e2dc11301675ee44304af2ce6faf362df493
Son Pham-Ba <son.phamba@epfl.ch>
2024-05-14 11:26:23 +0200

    feat: add tslong dask wrapper skeleton

--- tstore/tslong/__init__.py
+++ tstore/tslong/__init__.py
@@ -4,16 +4,20 @@ from pathlib import Path
 from typing import Union
 
 from tstore.backend import Backend
+from tstore.tslong.dask import TSLongDask
 from tstore.tslong.pandas import TSLongPandas
 from tstore.tslong.polars import TSLongPolars
+from tstore.tslong.pyarrow import TSLongPyArrow
 from tstore.tslong.tslong import TSLong
 
 
 def open_tslong(base_dir: Union[str, Path], *args, backend: Backend = "pandas", **kwargs):
     """Read a TStore file structure as a TSLong object."""
     ts_long_classes = {
+        "dask": TSLongDask,
         "pandas": TSLongPandas,
         "polars": TSLongPolars,
+        "pyarrow": TSLongPyArrow,
     }
 
     if backend not in ts_long_classes:
