0bbe908e9ba71c2281c58ad7daa113c5b2216c12
Son Pham-Ba <son.phamba@epfl.ch>
2024-05-21 16:16:32 +0200

    feat: add backend kwargs to change_backend function

--- tstore/backend.py
+++ tstore/backend.py
@@ -21,84 +21,86 @@ PyArrowDataFrame = pa.Table
 DataFrame = Union[DaskDataFrame, PandasDataFrame, PolarsDataFrame, PyArrowDataFrame]
 
 
-def change_backend(df: DataFrame, new_backend: Backend) -> DataFrame:
+def change_backend(df: DataFrame, new_backend: Backend, **backend_kwargs) -> DataFrame:
     """Change the backend of a dataframe."""
     if isinstance(df, DaskDataFrame):
-        return _change_backend_from_dask(df, new_backend)
+        return _change_backend_from_dask(df, new_backend, **backend_kwargs)
 
     if isinstance(df, PandasDataFrame):
-        return _change_backend_from_pandas(df, new_backend)
+        return _change_backend_from_pandas(df, new_backend, **backend_kwargs)
 
     if isinstance(df, PolarsDataFrame):
-        return _change_backend_from_polars(df, new_backend)
+        return _change_backend_from_polars(df, new_backend, **backend_kwargs)
 
     if isinstance(df, PyArrowDataFrame):
-        return _change_backend_from_pyarrow(df, new_backend)
+        return _change_backend_from_pyarrow(df, new_backend, **backend_kwargs)
 
     raise TypeError(f"Unsupported dataframe type: {type(df).__module__}.{type(df).__qualname__}")
 
 
-def _change_backend_from_dask(df: DaskDataFrame, new_backend: Backend) -> DataFrame:
+def _change_backend_from_dask(df: DaskDataFrame, new_backend: Backend, **backend_kwargs) -> DataFrame:
     """Change the backend of a Dask dataframe."""
     if new_backend == "dask":
         return df
 
     if new_backend == "pandas":
-        return df.compute()
+        return df.compute(**backend_kwargs)
 
     if new_backend == "polars":
-        return pl.DataFrame(df.compute())
+        return pl.from_pandas(df.compute(), **backend_kwargs)
 
     if new_backend == "pyarrow":
-        return pa.Table.from_pandas(df.compute())
+        return pa.Table.from_pandas(df.compute(), **backend_kwargs)
 
     raise ValueError(f"Unsupported backend: {new_backend}")
 
 
-def _change_backend_from_pandas(df: PandasDataFrame, new_backend: Backend) -> DataFrame:
+def _change_backend_from_pandas(df: PandasDataFrame, new_backend: Backend, **backend_kwargs) -> DataFrame:
     """Change the backend of a Pandas dataframe."""
     if new_backend == "dask":
-        return dd.from_pandas(df, npartitions=1)
+        return dd.from_pandas(df, **backend_kwargs)
 
     if new_backend == "pandas":
         return df
 
     if new_backend == "polars":
-        return pl.DataFrame(df)
+        return pl.from_pandas(df, **backend_kwargs)
 
     if new_backend == "pyarrow":
-        return pa.Table.from_pandas(df)
+        return pa.Table.from_pandas(df, **backend_kwargs)
 
     raise ValueError(f"Unsupported backend: {new_backend}")
 
 
-def _change_backend_from_polars(df: PolarsDataFrame, new_backend: Backend) -> DataFrame:
+def _change_backend_from_polars(df: PolarsDataFrame, new_backend: Backend, **backend_kwargs) -> DataFrame:
     """Change the backend of a Polars dataframe."""
     if new_backend == "dask":
-        return dd.from_pandas(df.to_pandas(), npartitions=1)
+        return dd.from_pandas(df.to_pandas(use_pyarrow_extension_array=True), **backend_kwargs)
 
     if new_backend == "pandas":
-        return df.to_pandas()
+        backend_kwargs.setdefault("use_pyarrow_extension_array", True)
+        return df.to_pandas(**backend_kwargs)
 
     if new_backend == "polars":
         return df
 
     if new_backend == "pyarrow":
-        return df.to_arrow()
+        return df.to_arrow(**backend_kwargs)
 
     raise ValueError(f"Unsupported backend: {new_backend}")
 
 
-def _change_backend_from_pyarrow(df: PyArrowDataFrame, new_backend: Backend) -> DataFrame:
+def _change_backend_from_pyarrow(df: PyArrowDataFrame, new_backend: Backend, **backend_kwargs) -> DataFrame:
     """Change the backend of a PyArrow table."""
     if new_backend == "dask":
-        return dd.from_pandas(df.to_pandas(), npartitions=1)
+        return dd.from_pandas(df.to_pandas(types_mapper=pd.ArrowDtype), **backend_kwargs)
 
     if new_backend == "pandas":
-        return df.to_pandas()
+        backend_kwargs.setdefault("types_mapper", pd.ArrowDtype)
+        return df.to_pandas(**backend_kwargs)
 
     if new_backend == "polars":
-        return pl.DataFrame(df.to_pandas())
+        return pl.from_arrow(df, **backend_kwargs)
 
     if new_backend == "pyarrow":
         return df
