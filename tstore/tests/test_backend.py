7875280629140313d97269bb90c908a61d79ab47
Son Pham-Ba <son.phamba@epfl.ch>
2024-06-04 09:02:37 +0200

    feat: add change backend functions for series

--- tstore/tests/test_backend.py
+++ tstore/tests/test_backend.py
@@ -10,9 +10,13 @@ from tstore import backend
 
 # Fixtures imported from conftest.py:
 # - dask_dataframe
+# - dask_series
 # - pandas_dataframe
+# - pandas_series
 # - polars_dataframe
+# - polars_series
 # - pyarrow_dataframe
+# - pyarrow_series
 
 
 dataframe_types = {
@@ -22,30 +26,41 @@ dataframe_types = {
     "pyarrow": pa.Table,
 }
 
+series_types = {
+    "dask": dd.Series,
+    "pandas": pd.Series,
+    "polars": pl.Series,
+    "pyarrow": pa.Array,
+}
+
 backend_names = list(dataframe_types.keys())
 
 
 def test_types() -> None:
     """Test the types defined in the backend module."""
     assert backend.DaskDataFrame is dd.DataFrame
+    assert backend.DaskSeries is dd.Series
     assert backend.PandasDataFrame is pd.DataFrame
+    assert backend.PandasSeries is pd.Series
     assert backend.PolarsDataFrame is pl.DataFrame
+    assert backend.PolarsSeries is pl.Series
     assert backend.PyArrowDataFrame is pa.Table
+    assert backend.PyArrowSeries is pa.Array
 
 
 @pytest.mark.parametrize("backend_to", backend_names)
 @pytest.mark.parametrize("backend_from", backend_names)
-def test_change_backend(
+def test_change_dataframe_backend(
     backend_from: backend.Backend,
     backend_to: backend.Backend,
     request,
 ) -> None:
-    """Test the change_backend function."""
+    """Test the change_backend function on dataframes."""
     dataframe_fixture_name = f"{backend_from}_dataframe"
     df = request.getfixturevalue(dataframe_fixture_name)
     assert isinstance(df, dataframe_types[backend_from])
 
-    df_new = backend.change_backend(df=df, new_backend=backend_to)
+    df_new = backend.change_backend(obj=df, new_backend=backend_to)
     assert isinstance(df_new, dataframe_types[backend_to])
 
     if backend_from == "dask":
@@ -60,6 +75,25 @@ def test_change_backend(
 
 @pytest.mark.parametrize("backend_to", backend_names)
 @pytest.mark.parametrize("backend_from", backend_names)
+def test_change_series_backend(
+    backend_from: backend.Backend,
+    backend_to: backend.Backend,
+    request,
+) -> None:
+    """Test the change_backend function on series."""
+    series_fixture_name = f"{backend_from}_series"
+    series = request.getfixturevalue(series_fixture_name)
+    assert isinstance(series, series_types[backend_from])
+
+    series_new = backend.change_backend(obj=series, new_backend=backend_to)
+    assert isinstance(series_new, series_types[backend_to])
+
+    # Check size
+    assert len(series) == len(series_new)
+
+
+@pytest.mark.parametrize("backend_to", backend_names)
+@pytest.mark.parametrize("backend_from", backend_names)
 def test_change_backend_to_and_fro(
     backend_from: backend.Backend,
     backend_to: backend.Backend,
@@ -68,8 +102,8 @@ def test_change_backend_to_and_fro(
     """Test the change_backend function with A -> B -> A and check for equality."""
     dataframe_fixture_name = f"{backend_from}_dataframe"
     df = request.getfixturevalue(dataframe_fixture_name)
-    df_temp = backend.change_backend(df=df, new_backend=backend_to)
-    df_new = backend.change_backend(df=df_temp, new_backend=backend_from)
+    df_temp = backend.change_backend(obj=df, new_backend=backend_to)
+    df_new = backend.change_backend(obj=df_temp, new_backend=backend_from)
 
     if backend_from == "dask":
         df = df.compute()
@@ -81,3 +115,16 @@ def test_change_backend_to_and_fro(
     # Check values
     # TODO: Polars and PyArrow don't keep the index column
     assert df.equals(df_new)
+
+
+def test_polars_to_pandas(polars_dataframe: pl.DataFrame) -> None:
+    """Test the .to_pandas() method on a Polars DataFrame with PyArrow dtype."""
+    # Check that both methods return the same DataFrame
+    df1 = polars_dataframe.to_pandas(use_pyarrow_extension_array=True)
+    df2 = polars_dataframe.to_pandas(types_mapper=pd.ArrowDtype)
+    pd.testing.assert_frame_equal(df1, df2)
+
+    # Check that dtypes are ArrowDtypes
+    for df in [df1, df2]:
+        for dtype in df.dtypes:
+            assert isinstance(dtype, pd.ArrowDtype)
